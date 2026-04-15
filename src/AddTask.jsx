e873497c2711b7e847a85c060c14e68d13a9d0a5
Simppa <simo.siren@careeria.fi>
2026-04-15 12:20:26 +0300

    tyylit

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -3,10 +3,39 @@ import React, {useState} from 'react'
 
 function AddTask() {
 
+// tyylimääritys
+const windowStyle = {
+    maxWidth: "30%",
+    marginBottom: "5%",
+    marginLeft: "35%",
+    padding: "20px",
+    borderStyle: "solid",
+    borderWidth: "2px",
+    borderColor: "black",
+    borderRadius: "15px",
+    textAlign: "left"
+}
+
 return(  
-    <div>
-        <h5>lisäyslomake</h5>
+    <div style={windowStyle}>
+        <h5>Add new Task</h5>
+
+
+        <form>
+            <label>Title</label>
+            <input type="text" placeholder='Title' />
+
+             <label>Description</label>
+            <input type="text" placeholder='Description' />
+
+            <label>Priority </label>
+            <input type="number" min={1} max={5} />
+            
+            <input type="submit" value="save" />
+
+        </form>
     </div>
+    
     )
 }
 
