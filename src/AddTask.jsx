329dd9ef18b583731c51e92368eec5277583dec8
Simppa <simo.siren@careeria.fi>
2026-04-15 12:50:01 +0300

    input statet

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -16,20 +16,27 @@ const windowStyle = {
     textAlign: "left"
 }
 
+// Statet, jotka pitää sisällään tiedon inputkenttien sisällöstä
+// koko ajan. Päivittyy aina kun käyttäjä muuttaa tekstiä inputkentässä
+const [newTitle, setNewTitle] = useState("")
+const [newDesc, setNewDesc] = useState("")
+const [newPrior, setNewprior] = useState(3)
+
+
+
 return(  
     <div style={windowStyle}>
         <h5>Add new Task</h5>
 
-
         <form>
             <label>Title</label>
-            <input type="text" placeholder='Title' />
+            <input type="text" value={newTitle} placeholder='Title' onChange={({target}) => setNewTitle(target.value)} />
 
              <label>Description</label>
-            <input type="text" placeholder='Description' />
+            <input type="text" value={newDesc} placeholder='Description' onChange={({target}) => setNewDesc(target.value)} />
 
             <label>Priority </label>
-            <input type="number" min={1} max={5} />
+            <input type="number" value={newPrior} min={1} max={5} onChange={({target}) => setNewPrior(target.value)} />
             
             <input type="submit" value="save" />
 
