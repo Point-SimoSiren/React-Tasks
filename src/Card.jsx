3d556d8a55babfa28b97b716c0bd4efcf259fd31
Simppa <simo.siren@careeria.fi>
2026-04-16 12:28:01 +0300

    delete pitäisi toimia

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,9 +4,45 @@ import React, {useState} from 'react'
 // Card komponentti saa propsina task objektin kutsuvalta Tasks.jsx komponentilta
 function Card({task}) {
 
+//---- Poistofunktio, jota delete nappi kutsuu -------------
+
+const poista = (task) => {
+    let vahvistus = window.confirm("Want to delete task: " + task.Title + "?")
+    if (vahvistus === false) {
+        return
+    }
+    // Http delete pyyntö backendiin
+    try {
+        let url = "https://taskbackend20264.azurewebsites.net/api/tasks" + "/" + task.taskId
+        const response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error("Virhe")
+        }
+
+        alert("Poisto onnistui!")
+
+    } catch (error) {
+        console.error("Virhe:", error)
+        alert("Poisto epäonnistui")
+    }
+
+//---Poisto päättyy--------------------------------------
+
+
 return(  
     <div className='kortti' key={task.taskId}>
-        <button style={{background: "red"}}>delete</button>
+        <button style={{background: "red", marginRight: "10px"}}
+        onClick={() => poista(task)}>
+            delete
+        </button>
+
+
         <button style={{background: "cyan", color: "blue"}}>edit</button>
 
         <h5>{task.title}</h5>
