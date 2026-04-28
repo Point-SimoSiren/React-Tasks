941da38d0c1be2d53e6a299f929bec9b8ebe0431
Simppa <simo.siren@careeria.fi>
2026-04-28 09:36:25 +0300

    api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,6 @@
-const API_BASE_URL = "https://localhost:7111"
+// const API_BASE_URL = "https://localhost:7111"
 
-export const TASKS_API_URL = `${API_BASE_URL}/api/tasks`
+// export const TASKS_API_URL = `${API_BASE_URL}/api/tasks`
+
+
+export const TASKS_API_URL = "https://localhost:7111/api/tasks"
